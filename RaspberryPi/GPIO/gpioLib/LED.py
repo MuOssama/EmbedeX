660c8b2222a53939314f8e3444154b890499a06a
Mustapha Ossama <109214165+MuOssama@users.noreply.github.com>
2024-01-23 23:53:50 +0200

    Add files via upload

---
+++ RaspberryPi/GPIO/gpioLib/LED.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as gpio
+import time
+led_pin = 3
+#set BCM convention
+gpio.setmode(gpio.BCM)
+
+#set pin3 as output
+gpio.setup(led_pin, gpio.OUT)
+
+#set led pin high and low with delay
+while True:
+	gpio.output(led_pin, gpio.HIGH)
+	time.sleep(1)
+	gpio.output(led_pin, gpio.LOW)
+	time.sleep(1)

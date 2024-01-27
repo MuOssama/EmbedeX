2dea1e3d41a9c55cbe3fcc75959c2cd8ed083b2a
MuOssama <mustaphaossama2000@gmail.com>
2024-01-28 01:34:10 +0200

    pwm led example updated

---
+++ RPi_Lib/PWM/pwmLED.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as gpio
+import time
+led_pin = 4
+#set BCM convention
+gpio.setmode(gpio.BCM)
+
+#set pin3 as output
+gpio.setup(led_pin, gpio.OUT)
+
+
+#set led pin pwm
+pwmFreq = 100 #1000 Hz which is 1/10000 sec as T = 1/F
+channel = gpio.PWM(led_pin, pwmFreq)
+while True:
+	for duty in range(0,101):
+		channel.start(duty)
+		print(F'Duty: {duty}')
+		time.sleep(0.1)
+	for duty in range(0,101):
+		channel.start(100-duty)
+		print(F'Duty: {100-duty}')
+		time.sleep(0.05)
+
